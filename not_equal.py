202e1bd900a6ed0c0d193ff4a0f043d6bcd465e1
Hridita-11 <hridita.nz@gmail.com>
2026-02-10 19:35:47 +1300

    Add files via upload

---
+++ not_equal.py
@@ -0,0 +1,17 @@
+a=5 
+b=6 
+c=6
+print(a !=b)
+print(b !=c)
+
+a= "hello"
+b= "there"
+if a !=b:
+    print(a, "and" ,b, "are different" )
+
+a = 4
+b=  6
+if (a==1) != (b==6):
+    print("hello")
+
+
